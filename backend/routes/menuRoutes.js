dd7fe941eb50d221921efdf367b6fdc19a2321c6
Sonamkumari-git <7258sonam@gmail.com>
2026-07-06 12:30:22 +0530

    Update menuRoutes.js

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { addMenuItem, getMenuItems, updateMenuItem, deleteMenuItem, toggleStock } = require('../controllers/menuController');
 
-router.post('/add', addMenuItem);
-router.get('/all', getMenuItems);
-router.put('/update/:id', updateMenuItem);
-router.delete('/delete/:id', deleteMenuItem);
-router.patch('/toggle-stock/:id', toggleStock);
+// Dhyan do: Yahan '/all' ki jagah '/' hona chahiye
+router.get('/', getMenuItems);               
+router.post('/', addMenuItem);              
+router.put('/:id', updateMenuItem);          
+router.delete('/:id', deleteMenuItem);       
+router.patch('/toggle-stock/:id', toggleStock); 
 
 module.exports = router;
